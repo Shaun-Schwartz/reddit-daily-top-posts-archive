4580ad46b6c2f2ec945466b7ba1520c84c41322d
Shaun Schwartz <me@shaunschwartz.com>
2019-10-29 14:15:50 -0700

    remove sidekiq initializer

--- config/initializers/sidekiq.rb
+++
@@ -1,8 +0,0 @@
-sidekiq_url = Rails.application.config.redis[:url] + Rails.application.config.redis[:sidekiq_path] + '_' + Rails.env
-Sidekiq.configure_server do |config|
-  config.redis = { url: sidekiq_url }
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { url: sidekiq_url }
-end

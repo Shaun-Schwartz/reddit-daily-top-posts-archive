d33da686aac9bcae220edc99cd16b68e54e81118
Shaun Schwartz <me@shaunschwartz.com>
2019-10-29 15:59:28 -0700

    add production puma config

--- config/puma.rb
+++ config/puma.rb
@@ -36,3 +36,36 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+current_env = ENV.fetch("RAILS_ENV")
+puts '--- CURRENT ENVIRONMENT ---'
+puts current_env
+puts '--- CURRENT ENVIRONMENT ---'
+if current_env == "production"
+  # Change to match your CPU core count
+  workers 2
+  # Min and Max threads per worker
+  threads 1, 6
+  app_dir = File.expand_path("../..", __FILE__)
+  shared_dir = "#{app_dir}/shared"
+  # Default to production
+  rails_env = "production"
+  environment rails_env
+
+  # Set up socket location
+  bind "unix://#{shared_dir}/sockets/puma.sock"
+
+  # Logging
+  stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+
+  # Set master PID and state locations
+  pidfile "#{shared_dir}/pids/puma.pid"
+  state_path "#{shared_dir}/pids/puma.state"
+  activate_control_app
+
+  on_worker_boot do
+    require "active_record"
+    ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+    ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+  end
+end

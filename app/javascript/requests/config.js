96e6ae84a4f21e98cd55fe1f215c655ea92e0bd9
Shaun Schwartz <me@shaunschwartz.com>
2019-11-16 14:15:18 -0800

    set api_url depending on environment

--- app/javascript/requests/config.js
+++ app/javascript/requests/config.js
@@ -1,1 +1,8 @@
-export const BASE_URL = 'http://localhost:3000/api';
+const hostname = window.location.hostname;
+var api_url;
+if (hostname === 'localhost') {
+  api_url = 'http://localhost:3000/api';
+} else {
+  api_url = 'https://topofreddit.shaunschwartz.com/api';
+}
+export const BASE_URL = api_url;

379c677d84e240bfca231585ec9a64af8eaa0282
Shaun Schwartz <me@shaunschwartz.com>
2019-11-16 14:19:46 -0800

    delete old controller, and views

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,9 @@ Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
   root 'client#index'
 
-  match '/client/*path', to: 'client#index', via: :all
-
   namespace :api, defaults: {format: :json} do
     resources :reddit_posts
     get '/subreddits', to: 'reddit_posts#subreddits'
   end
-  
+
 end
